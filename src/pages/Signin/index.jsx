165d5718e2407fe209e2abe1f5b7b900805dbd24
Matheus <matheus-rodrigues37@live.com>
2022-11-25 17:32:18 -0300

    styling

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,9 +1,28 @@
-import { Container } from './styles';
+import { ReactComponent as Logo } from '../../assets/svg/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/InputForm';
+import { Container, Content, Form } from './styles';
 
 function Signin() {
   return (
     <Container>
-      <h1>Oi</h1>
+      <Logo />
+      <Content>
+        <h1>Faça login</h1>
+        <Form>
+          <Input
+            label="Email"
+            placeholder="Exemplo: exemplo@exemplo.com.br"
+            type="text"
+          />
+          <Input
+            label="Senha"
+            placeholder="Exemplo: exemplo@exemplo.com.br"
+            type="password"
+          />
+          <Button text="Entrar" />
+        </Form>
+      </Content>
     </Container>
   );
 }
--- src/pages/Signin/styles.js
+++ src/pages/Signin/styles.js
@@ -1,3 +1,26 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  gap: 20rem;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 4rem;
+  background-color: ${({ theme }) => theme.colors.backgroundForm};
+  min-width: 300px;
+
+  > h1 {
+    font-family: ${({ theme }) => theme.fonts.title};
+    color: ${({ theme }) => theme.colors.white};
+    text-align: center;
+  }
+`;
+
+export const Form = styled.form``;
